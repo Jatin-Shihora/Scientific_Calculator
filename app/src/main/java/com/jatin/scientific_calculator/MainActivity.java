e63271986a53c4de95c93eda9bff811f0f1a6ae6
Jatin Shihora <Jatinshihora0123@gmail.com>
2021-02-19 00:15:01 +0530

    Added comments to the project wherever felt necessary

--- app/src/main/java/com/jatin/scientific_calculator/MainActivity.java
+++ app/src/main/java/com/jatin/scientific_calculator/MainActivity.java
@@ -2,12 +2,12 @@ package com.jatin.scientific_calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
 
 TextView user_input,sign_Box;
 Double num1,num2,answer;
@@ -21,21 +21,34 @@ boolean has_dot;
 
         user_input=(TextView) findViewById(R.id.input_user);
         sign_Box=(TextView) findViewById(R.id.sign_user);
-
         has_dot=false;
     }
 
+//@SuppressLint("SetTextI18n")--->>  "I18" stands for "Internationalization". Android's localized resources mechanism allows you to support a variety of locales without having to modify your code.If your application only supports certain symbols like +,-,X etc  symbols then it makes sense to hardcode it and use @SuppressLint("SetTextI18n") to silence the warning
+
+   /*making functions for each button in calcualtor*/
+    @SuppressLint("SetTextI18n")
     public void btn_0(View view){ user_input.setText(user_input.getText()+"0"); }
+    @SuppressLint("SetTextI18n")
     public void btn_1(View view){ user_input.setText(user_input.getText()+"1"); }
+    @SuppressLint("SetTextI18n")
     public void btn_2(View view){ user_input.setText(user_input.getText()+"2"); }
+    @SuppressLint("SetTextI18n")
     public void btn_3(View view){ user_input.setText(user_input.getText()+"3"); }
+    @SuppressLint("SetTextI18n")
     public void btn_4(View view){ user_input.setText(user_input.getText()+"4"); }
+    @SuppressLint("SetTextI18n")
     public void btn_5(View view){ user_input.setText(user_input.getText()+"5"); }
+    @SuppressLint("SetTextI18n")
     public void btn_6(View view){ user_input.setText(user_input.getText()+"6"); }
+    @SuppressLint("SetTextI18n")
     public void btn_7(View view){ user_input.setText(user_input.getText()+"7"); }
+    @SuppressLint("SetTextI18n")
     public void btn_8(View view){ user_input.setText(user_input.getText()+"8"); }
+    @SuppressLint("SetTextI18n")
     public void btn_9(View view){ user_input.setText(user_input.getText()+"9"); }
 
+    @SuppressLint("SetTextI18n")
     public void btn_dot(View view)//if dot is not present than give 0. else give value_a.value_b
      {
         if(!has_dot){
@@ -47,15 +60,15 @@ boolean has_dot;
          has_dot=true;
         }
     }
-
+    @SuppressLint("SetTextI18n")
     public void btn_add(View view){
-        sign="+";
-        val_1=user_input.getText().toString();
-        user_input.setText(null);
+        sign="+";     //we are assigning sign to every buuton because we will be using switch case in that                       expression will be sign.
+        val_1=user_input.getText().toString();//once the user enters any sign
+        user_input.setText(null);             //the text sets to null
         sign_Box.setText("+");
         has_dot=false;
     }
-
+    @SuppressLint("SetTextI18n")
     public void btn_subtract(View view){
         sign="-";
         val_1=user_input.getText().toString();
@@ -63,7 +76,7 @@ boolean has_dot;
         sign_Box.setText("-");
         has_dot=false;
     }
-
+    @SuppressLint("SetTextI18n")
     public void btn_multiply(View view){
         sign="X";
         val_1=user_input.getText().toString();
@@ -71,7 +84,7 @@ boolean has_dot;
         sign_Box.setText("X");
         has_dot=false;
     }
-
+    @SuppressLint("SetTextI18n")
     public void btn_divide(View view){
         sign="%";
         val_1=user_input.getText().toString();
@@ -79,7 +92,7 @@ boolean has_dot;
         sign_Box.setText("%");
         has_dot=false;
     }
-
+    @SuppressLint("SetTextI18n")
     public void btn_log(View view){
         sign="log";
         val_1=user_input.getText().toString();
@@ -87,7 +100,7 @@ boolean has_dot;
         sign_Box.setText("log");
         has_dot=false;
     }
-
+    @SuppressLint("SetTextI18n")
     public void btn_ln(View view){
         sign="ln";
         val_1=user_input.getText().toString();
@@ -95,7 +108,7 @@ boolean has_dot;
         sign_Box.setText("ln");
         has_dot=false;
     }
-
+    @SuppressLint("SetTextI18n")
     public void btn_power(View view){
         sign="power";
         val_1=user_input.getText().toString();
@@ -103,7 +116,7 @@ boolean has_dot;
         sign_Box.setText("xⁿ");
         has_dot=false;
     }
-
+    @SuppressLint("SetTextI18n")
     public void btn_factorial(View view){
         sign="factorial";
         val_1=user_input.getText().toString();
@@ -111,7 +124,7 @@ boolean has_dot;
         sign_Box.setText("!");
         has_dot=false;
     }
-
+    @SuppressLint("SetTextI18n")
     public void btn_sin(View view){
         sign="sin";
         val_1=user_input.getText().toString();
@@ -119,7 +132,7 @@ boolean has_dot;
         sign_Box.setText("sin");
         has_dot=false;
     }
-
+    @SuppressLint("SetTextI18n")
     public void btn_cos(View view){
         sign="cos";
         val_1=user_input.getText().toString();
@@ -127,7 +140,7 @@ boolean has_dot;
         sign_Box.setText("cos");
         has_dot=false;
     }
-
+    @SuppressLint("SetTextI18n")
     public void btn_tan(View view){
         sign="tan";
         val_1=user_input.getText().toString();
@@ -135,7 +148,7 @@ boolean has_dot;
         sign_Box.setText("tan");
         has_dot=false;
     }
-
+    @SuppressLint("SetTextI18n")
     public void btn_root(View view){
         sign="root";
         val_1=user_input.getText().toString();
@@ -144,20 +157,24 @@ boolean has_dot;
         has_dot=false;
     }
 
-  /*  public void btn_delete(View view){
+  /*  @SuppressLint("SetTextI18n")
+  public void btn_delete(View view){
         if(user_input.getText().equals("")){
         user_input.setText(null);
         }else{
         int len=user_input.getText().length();
         String s=user_input.getText().toString();
-            if(s.charAt (len-1)=="."){
+            if(s.charAt (len-1)==".")
+            {
                 has_dot=false;
                 user_input.setText(user_input.getText().subSequence(0,user_input.getText().length() - 1));
             }else
             {user_input.setText(user_input.getText().subSequence(0,user_input.getText().length() - 1)); }
         }
     }*/
-    public void btn_clear(View view){
+  @SuppressLint("SetTextI18n")
+    public void btn_clear(View view)//this fuction clears all the inputs and outputs
+  {
     user_input.setText(null);
     sign_Box.setText(null);
     val_1=null;
@@ -166,20 +183,26 @@ boolean has_dot;
     has_dot=false;
     }
 
-    public void btn_equal(View view){
+    @SuppressLint("SetTextI18n")
+    public void btn_equal(View view)
+    {
+        //if there is no sign,or no input than it should give error
     if(sign==null){sign_Box.setText("Error!");}
     else if (user_input.getText().equals("")){sign_Box.setText("Error!");}
    /* else if ((sign.equals("+")) || (sign.equals("-")) || (sign.equals("X")) || (sign.equals("%")) && (val_1.equals("")))
         {
             sign_Box.setText("Error!");
         }*/
+
     else{
-        switch (sign){
+        switch (sign)//since in every fuction we have initialized sign, so now we will be using it as a expression in switch case statement
+        {
             default:
                 break;
             case "log":
                 val_1=user_input.getText().toString();
-                num1=Double.parseDouble(val_1);
+                num1=Double.parseDouble(val_1);//here we have used wrapper class to wrap primitive data type
+//since the parsedouble value should have a argument represented in string therefore we have converted val_1 tostring in above line
                 user_input.setText(Math.log10(num1)+"");
                 sign=null;
                 sign_Box.setText(null);
